e8b7aae4fac8bd228de3a045732ea4f530854e01
Tavi007 <Tavi007@users.noreply.github.com>
2021-01-10 17:39:15 +0100

    now extends ItemStackHandler

--- src/main/java/Tavi007/Materia/inventory/EquippingStationItemHandler.java
+++ src/main/java/Tavi007/Materia/inventory/EquippingStationItemHandler.java
@@ -2,97 +2,62 @@ package Tavi007.Materia.inventory;
 
 import java.util.ArrayList;
 
-import Tavi007.Materia.Materia;
 import Tavi007.Materia.inventory.container.EquippingStationContainer;
-import Tavi007.Materia.items.BaseMateria;
 import Tavi007.Materia.items.IMateriaTool;
 import Tavi007.Materia.util.MateriaToolUtil;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 
-public class EquippingStationItemHandler implements IItemHandler {
+public class EquippingStationItemHandler extends ItemStackHandler {
 	private final EquippingStationContainer container;
 
-	private final ArrayList<ItemStack> stackList = new ArrayList<ItemStack>();
-
-
 	public EquippingStationItemHandler(EquippingStationContainer container) {
-		for (int i=0; i<9; i++) {
-			stackList.add(ItemStack.EMPTY);
-		}
+		super(9);
 		this.container = container;
 	}
-
-
+	
 	@Override
-	public int getSlots() {
-		return stackList.size();
-	}
-
-
-	@Override
-	public ItemStack getStackInSlot(int slot) {
-		return stackList.get(slot);
-	}
-
-
-	@Override
-	public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
-		ItemStack stackInSlot = getStackInSlot(slot);
-		if (stackInSlot.isEmpty()) {
-			stackList.set(slot, stack);
-		}
-		if(!stack.isEmpty()) {
-			if(stack.getItem() instanceof IMateriaTool) {
-				//get Materia  Tool and set stackList accordingly
-				IMateriaTool tool = (IMateriaTool) stack.getItem();
-				ArrayList<ItemStack> topStacks = MateriaToolUtil.getMateriaStacks(tool.getTopSlots());
-				for(int i=0; i<topStacks.size(); i++) {
-					stackList.set(i, topStacks.get(i));
-				}
-				ArrayList<ItemStack> botStacks = MateriaToolUtil.getMateriaStacks(tool.getBotSlots());
-				for(int i=0; i<botStacks.size(); i++) {
-					stackList.set(i+4, botStacks.get(i));
-				}
-				
-			}
-			else if (stack.getItem() instanceof BaseMateria) {
-				//add to tool and recalculate effects
-				IMateriaTool tool = (IMateriaTool) getMateriaToolStack().getItem(); //the tool
-				MateriaToolUtil.setMateriaStack(tool, stack, slot);
-				
-			}
-			else {
-				//should not have happened
-				Materia.LOGGER.warn("ItemStack was not supposed to be inserted. The ItemStack was " + stack.getItem().getName().getString());
-			}
-		}
-
-		return stackInSlot;
-	}
-
-
-	@Override
-	public ItemStack extractItem(int slot, int amount, boolean simulate) {
-		ItemStack stack = getStackInSlot(slot).getStack();
-		if(!stack.isEmpty()) {
-			if(stack.getItem() instanceof IMateriaTool) {
-				//clear Materia inventory
-				for(int i=0; i<8; i++) {
-					stackList.set(i, ItemStack.EMPTY);
-				}
-			}
-			else if (stack.getItem() instanceof BaseMateria) {
-				//remove from tool and recalculate effects
+	public void onContentsChanged(int slot) {
+		if(slot>=0 && slot < 8) {
+			// BaseMateria
+			ItemStack stack = getStackInSlot(slot);
+			if (stack.isEmpty()) {
+				// Materia got removed
 				IMateriaTool tool = (IMateriaTool) getMateriaToolStack().getItem(); //the tool
 				MateriaToolUtil.setMateriaStack(tool, ItemStack.EMPTY, slot);
 			}
 			else {
-				//should not have happened
-				Materia.LOGGER.warn("ItemStack was not supposed to be in this Slot. The ItemStack was " + stack.getItem().getName().getString());
+				// Materia got inserted
+				IMateriaTool tool = (IMateriaTool) getMateriaToolStack().getItem(); //the tool
+				MateriaToolUtil.setMateriaStack(tool, stack, slot);
+			}
+			
+		}
+		else if(slot == 8) {
+			// IMateriaTool
+			ItemStack stack = getStackInSlot(slot);
+			if (stack.isEmpty()) {
+				// IMateriaTool got removed
+				for(int i=0; i<8; i++) {
+					stacks.set(i, ItemStack.EMPTY);
+				}
+			}
+			else {
+				// IMateriaTool got inserted
+				IMateriaTool tool = (IMateriaTool) stack.getItem();
+				ArrayList<ItemStack> topStacks = MateriaToolUtil.getMateriaStacks(tool.getTopSlots());
+				for(int i=0; i<topStacks.size(); i++) {
+					stacks.set(i, topStacks.get(i));
+				}
+				ArrayList<ItemStack> botStacks = MateriaToolUtil.getMateriaStacks(tool.getBotSlots());
+				for(int i=0; i<botStacks.size(); i++) {
+					stacks.set(i+4, botStacks.get(i));
+				}
 			}
 		}
-		return stack;
+		else {
+			// Should not have happened
+		}
 	}
 
 
@@ -130,6 +95,6 @@ public class EquippingStationItemHandler implements IItemHandler {
 	}
 	
 	public ItemStack getMateriaToolStack() {
-		return stackList.get(8);
+		return stacks.get(8);
 	}
 }
