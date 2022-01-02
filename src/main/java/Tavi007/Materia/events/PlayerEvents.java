3bd020e11ca46b18b8a0e077c06287734ae17e45
Tavi007 <Tavi007@users.noreply.github.com>
2022-01-02 21:41:05 +0100

    prepare event for break speed adjusting

--- src/main/java/Tavi007/Materia/events/PlayerEvents.java
+++ src/main/java/Tavi007/Materia/events/PlayerEvents.java
@@ -2,7 +2,9 @@ package Tavi007.Materia.events;
 
 import Tavi007.Materia.Materia;
 import Tavi007.Materia.items.IMateriaTool;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.AnvilUpdateEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -15,6 +17,15 @@ public class PlayerEvents {
 		if(event.getLeft() != null && event.getLeft().getItem() instanceof IMateriaTool
 		&& event.getRight() != null && !event.getRight().isEnchanted()) {
 			event.setCanceled(true);
+		}
+	}
+
+	@SubscribeEvent
+	public static void adjustBreakSpeed(PlayerEvent.BreakSpeed event) {
+		ItemStack stack = event.getPlayer().getActiveItemStack();
+		if(stack.getItem() instanceof IMateriaTool) {
+			float factor = 1.0f; // TODO: get factor from a HelperMethod. It will calculate one depending on Efficiency UP materia
+			event.setNewSpeed(event.getOriginalSpeed()*factor);
 		}
 	}
 }
