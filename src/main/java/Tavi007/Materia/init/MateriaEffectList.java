9c3b2cddf9640de8c31edf20fe1233ff1c3aaad5
Tavi007 <Tavi007@users.noreply.github.com>
2022-01-08 22:06:31 +0100

    renamed file

--- src/main/java/Tavi007/Materia/init/MateriaEffectList.java
+++ src/main/java/Tavi007/Materia/init/MateriaEffectList.java
@@ -1,18 +1,18 @@
 package Tavi007.Materia.init;
 
 import Tavi007.Materia.effects.Element;
-import Tavi007.Materia.effects.EffectRegistry;
+import Tavi007.Materia.effects.MateriaEffectRegistry;
 import Tavi007.Materia.effects.SpellEffect;
 import Tavi007.Materia.effects.Stat;
 import Tavi007.Materia.effects.StatUpEffect;
 
 public class MateriaEffectList {
 
-	public static void init() {
-		EffectRegistry.register(new SpellEffect(Element.FIRE));
-		EffectRegistry.register(new SpellEffect(Element.ICE));
-		
-		EffectRegistry.register(new StatUpEffect(Stat.MINING_SPEED));
-	}
-	
+    public static void init() {
+        MateriaEffectRegistry.register(new SpellEffect(Element.FIRE));
+        MateriaEffectRegistry.register(new SpellEffect(Element.ICE));
+
+        MateriaEffectRegistry.register(new StatUpEffect(Stat.MINING_SPEED));
+    }
+
 }
