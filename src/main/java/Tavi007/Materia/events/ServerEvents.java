28b73174e6247fed588835c9b46329e27bd3b911
Tavi007 <Tavi007@users.noreply.github.com>
2023-08-06 12:06:22 +0200

    added event to drop ap orbs

--- src/main/java/Tavi007/Materia/events/ServerEvents.java
+++ src/main/java/Tavi007/Materia/events/ServerEvents.java
@@ -1,9 +1,26 @@
 package Tavi007.Materia.events;
 
 import Tavi007.Materia.Materia;
+import Tavi007.Materia.entities.AbilityPointOrb;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
 @Mod.EventBusSubscriber(modid = Materia.MOD_ID, bus = Bus.FORGE)
 public class ServerEvents {
+
+    @SubscribeEvent
+    public static void onLivingEntityDeath(LivingDeathEvent event) {
+        Materia.LOGGER.info("entity died");
+
+        LivingEntity entity = event.getEntity();
+        if (entity.getLevel()instanceof ServerLevel serverLevel) {
+            int amount = 10; // TODO: use data from reload listener
+            AbilityPointOrb.award(serverLevel, entity.getPosition(0), amount);
+        }
+    }
+
 }
