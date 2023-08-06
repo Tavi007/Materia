3842f3a12d6fc5c6c9fc070df502a862b3960d54
Tavi007 <Tavi007@users.noreply.github.com>
2023-08-06 18:57:52 +0200

    remove debug way of leveling up materia

--- src/main/java/Tavi007/Materia/items/MateriaItem.java
+++ src/main/java/Tavi007/Materia/items/MateriaItem.java
@@ -4,13 +4,9 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
-import Tavi007.Materia.capabilities.level.LevelData;
 import Tavi007.Materia.util.CapabilityHelper;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -52,15 +48,6 @@ public class MateriaItem extends Item {
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
         CapabilityHelper.getLevelData(stack).appendHoverText(tooltip);
-    }
-
-    // testing
-    @Override
-    public InteractionResultHolder<ItemStack> use(Level worldIn, Player playerIn, InteractionHand handIn) {
-        ItemStack stack = playerIn.getItemInHand(handIn);
-        LevelData data = CapabilityHelper.getLevelData(stack);
-        data.addAP(3);
-        return InteractionResultHolder.success(playerIn.getItemInHand(handIn));
     }
 
     @Override
