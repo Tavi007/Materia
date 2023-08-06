4c6efe029475a53f36ded2f66f87bbc2ce1ee4b6
Tavi007 <Tavi007@users.noreply.github.com>
2023-08-06 18:18:35 +0200

    added configs for ap bottle

--- src/main/java/Tavi007/Materia/ServerConfig.java
+++ src/main/java/Tavi007/Materia/ServerConfig.java
@@ -14,6 +14,9 @@ public class ServerConfig {
     private final IntValue baseMobApAmount;
     private final DoubleValue mobApDropPenaltyScaling;
 
+    private final IntValue apBottleAmount;
+    private final IntValue apBottleRandomPercent;
+
     static {
         Pair<ServerConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ServerConfig::new);
         CONFIG_SPEC = specPair.getRight();
@@ -23,11 +26,18 @@ public class ServerConfig {
     ServerConfig(ForgeConfigSpec.Builder builder) {
         baseMobApAmount = builder
             .comment("The default ap Amount for any mob, if not provided by a datapack.")
-            .defineInRange("baseMobApAmount", 100, 0, Integer.MAX_VALUE);
+            .defineInRange("baseMobApAmount", 100, 0, 100_000_000);
         mobApDropPenaltyScaling = builder
             .comment(
                 "Scale the ap drop penalty for killing to many mobs of the same type in a short time period. 0 disables the penalty, >1 increases it.")
             .defineInRange("scaleMobApDropPenalty", 1.0, 0, 10.0);
+        apBottleAmount = builder
+            .comment("The amount of Ability Points dropped by the Bottle of Ability Points. Amount will vary depending on the apBottleRandomPercent.")
+            .defineInRange("apBottleAmount", 100, 0, 1_000_000);
+        apBottleRandomPercent = builder
+            .comment(
+                "Applies some randomness to the amount of Ability Points from the Bottle. If the bottle amount is 100 and the random percent is 10, the bottle will drop between 90 and 110 AP.")
+            .defineInRange("apBottleRandomPercent", 10, 0, 100);
     }
 
     public static int getBaseMobApAmount() {
@@ -36,5 +46,15 @@ public class ServerConfig {
 
     public static double getMobApDropPenaltyScaling() {
         return SERVER.mobApDropPenaltyScaling.get();
+    }
+
+    public static int getApBottleMinAmount() {
+        float lowerBoundPercent = ((float) 100 - SERVER.apBottleRandomPercent.get()) / 100;
+        return (int) (lowerBoundPercent * SERVER.apBottleAmount.get());
+    }
+
+    public static int getApBottleMaxAmount() {
+        float lowerBoundPercent = ((float) 100 + SERVER.apBottleRandomPercent.get()) / 100;
+        return (int) (lowerBoundPercent * SERVER.apBottleAmount.get());
     }
 }
